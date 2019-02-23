61dbe436797dfb5c78b89cceeda01bf0ceee8c06
dylen <dylenfu@126.com>
2019-02-23 14:27:38 +0800

    spring boot study

---
+++ src/main/java/org/dylenfu/springbootdating/SpringbootDatingApplication.java
@@ -0,0 +1,13 @@
+package org.dylenfu.springbootdating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dylenfu/springbootdating/SpringbootDatingApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dylenfu.springbootdating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDatingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
